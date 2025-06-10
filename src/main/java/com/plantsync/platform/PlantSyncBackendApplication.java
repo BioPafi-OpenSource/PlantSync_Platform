520795aa40a55683f7a65d8567ce130f7419974b
Renzo <u20231d974@upc.edu.pe>
2025-06-10 16:33:41 -0500

    Initial commit: Created Spring Project

---
+++ src/main/java/com/plantsync/platform/PlantSyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.plantsync.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantSyncBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantSyncBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plantsync/platform/PlantSyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantsync.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantSyncBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
