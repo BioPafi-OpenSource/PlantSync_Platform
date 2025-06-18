a96aa5bc50c2894608684070461044b0f0e7cba3
Renzo <u20231d974@upc.edu.pe>
2025-06-18 02:51:07 -0500

    feat: add shared bounded context
    Added aggregate and entity models in shared bounded context, added snake case naming strategy

---
+++ src/main/java/com/plantsync/platform/shared/infrastructure/flyway/FlywayConfiguration.java
@@ -0,0 +1,22 @@
+
+package com.plantsync.platform.shared.infrastructure.flyway;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Renzo Rivera U20231D974
+ */
+
+@Configuration
+public class FlywayConfiguration {
+
+    @Autowired
+    public FlywayConfiguration(DataSource dataSource) {
+        Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+    }
+}
+
